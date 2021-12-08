3dfe4655165bd73ebc24bb77304a66758c7b06aa
San Light <it4san@gmail.com>
2021-12-09 00:30:27 +0300

    Initial Commit

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        String s1 = "Some";
+        String s2 = "Stupid";
+        String s3 = "String";
+        String space = " ";
+
+        String res = s1 + space + s2 + space + s3;
+
+        System.out.println(res);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 200561;
+        short myShort = 32;
+        long myLong = 543765234;
+
+        double myDouble = 234.45; // 64 bits
+        float myFloat = 433.32f; // 32 bits
+
+        char c = 'F';
+        boolean b = true;
+
+        byte myByte = 125; // - 128 .. + 127
+
+        System.out.println(myInt);
+    }
+}
